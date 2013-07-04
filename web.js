c26d00e07d1469a44526dc6cf89c7c7b731b33a1
vagrant <camiespia@gmail.com>
2013-07-04 08:47:35 +0000

    Even more tests. Testopia.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var buffer = fs.readFileSync(index.html);
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
