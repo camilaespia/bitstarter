46adf50623150cb2bc021245f2e0281f2fbab796
vagrant <camiespia@gmail.com>
2013-07-08 21:41:00 +0000

    More testing.

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var bu = fs.readFileSync(index.html);
+var buffer = new Buffer("", "utf-8");
+
+buffer.write(fs.readFile(index.html));
 
 app.get('/', function(request, response) {
-  response.send(bu.toString('utf8', 0, len));
+  response.send(buffer.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
