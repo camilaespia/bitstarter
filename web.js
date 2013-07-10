a0a5b829b4331ed3b646f5acdb4f487489c8971a
vagrant <camiespia@gmail.com>
2013-07-10 00:48:19 +0000

    Testing testing testing

--- web.js
+++ web.js
@@ -3,10 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = fs.readFileSync('index.html', 'utf8');
-
-app.get('/', function(request, response) {
-  response.send(fs.writeFileSync('index.html', text, 'utf8'));
-});
+fs.writeFileSync('index.html', text, 'utf8');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
