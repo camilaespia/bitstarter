466bed7906b8c2dfce4be876c0224f98d90b5d95
vagrant <camiespia@gmail.com>
2013-07-10 00:54:03 +0000

    testing never ends

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = fs.readFileSync('index.html', 'utf8');
-fs.writeFileSync('index.html', text, 'utf8');
+
+app.get('/', function(request, response) {
+  response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
