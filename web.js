601c6522833a1d6db87f3d6d0ca640837f6d27cb
vagrant <camiespia@gmail.com>
2013-07-10 01:01:34 +0000

    halp

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync('index.html', 'utf8');
+var buf = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send('blargh');
 });
 
 var port = process.env.PORT || 5000;
